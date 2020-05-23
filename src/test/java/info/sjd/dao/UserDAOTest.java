2b7db0db2a9dabb1e40600f5b41de4c6fc0b04a9
Alex <ignatenko2207@gmail.com>
2020-05-23 21:33:09 +0300

    remove commented code

--- src/test/java/info/sjd/dao/UserDAOTest.java
+++ src/test/java/info/sjd/dao/UserDAOTest.java
@@ -1,26 +1,15 @@
 package info.sjd.dao;
 
-import info.sjd.EShopRsApplication;
 import info.sjd.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
-//@ContextConfiguration(classes = application)
-//@Import(UserDAOTest.class)
-//@SpringJUnitConfig(UserDAOTest.Config.class)
 class UserDAOTest {
-
-//    @Configuration
-//    static class Config {}
 
     @Autowired
     private UserDAO userDAO;
